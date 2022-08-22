2709f9cbd557e32f1e25711f2fe9dd104a177828
Adam Ligart <ligartadam@gmail.com>
2022-08-23 00:52:13 +0200

    Pages and components
    Created home page, added navbar component.

---
+++ my-app/src/Components/Navbar.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+import "./Navbar.css";
+import styled from 'styled-components'
+
+const First = styled.div``
+const Second = styled.div``
+const Third = styled.div``
+
+function Navbar() {
+  return (
+    <div className='navbar-container'>
+      <div className='navbar-wrapper'>
+        <First>First</First>
+        <Second>Second</Second>
+        <Third>Third</Third>
+      </div>
+    </div>
+  )
+}
+
+export default Navbar
