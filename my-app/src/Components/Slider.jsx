a6f23cbacfc460a5dd80199892d73f0ae2b0a158
Adam Ligart <ligartadam@gmail.com>
2022-08-24 01:06:45 +0200

    Slider: added image container, title, description.

--- my-app/src/Components/Slider.jsx
+++ my-app/src/Components/Slider.jsx
@@ -2,6 +2,7 @@ import styled from 'styled-components'
 import "./Slider.css";
 import KeyboardDoubleArrowLeft from '@mui/icons-material/KeyboardDoubleArrowLeftOutlined';
 import KeyboardDoubleArrowRight from '@mui/icons-material/KeyboardDoubleArrowRightOutlined';
+import mainLogo from'./logo_szakdoga.PNG';
 
 const Container = styled.div``
 const Arrow = styled.div`
@@ -15,18 +16,31 @@ const Wrapper = styled.div`
 const Slide = styled.div`
     display: flex;
     align-items: center;
+    width: 100vw;
+    height: 100vh;
 `;
 const ImageContainer = styled.div`
+    height: 100%;
     flex: 1;
-
 `;
 
 const Image = styled.img`
-
+    height: 80%;
 `;
 const InfoContainer = styled.div`
     flex: 1;
+    padding:50px;
 `;
+const Title = styled.h1`
+    font-size: 64px;
+
+`
+const Description = styled.p`
+    margin: 30px 0px;
+    font-size: 24px;
+    font-weight: 300;
+`
+const Button = styled.button``
 
 const Slider = () => {
   return (
@@ -35,10 +49,16 @@ const Slider = () => {
             <KeyboardDoubleArrowLeft/>
         </Arrow>
         <Wrapper>
-            <ImageContainer>
-                <Image />
-            </ImageContainer>
-            <InfoContainer></InfoContainer>
+            <Slide>
+                <ImageContainer>
+                    <Image src={mainLogo} />
+                </ImageContainer>
+                <InfoContainer>
+                    <Button className='btn-slider'>Részletek</Button>
+                    <Title>Teszt borválogatás</Title>
+                    <Description>Rövid leírás a termékről ...</Description>
+                </InfoContainer>
+            </Slide>
         </Wrapper>
         <Arrow direction="right" className='slider-arrow'>
             <KeyboardDoubleArrowRight/>
