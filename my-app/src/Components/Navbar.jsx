57d82e3754678e23a703fc251bef2b08c6fa9c24
Adam Ligart <ligartadam@gmail.com>
2022-08-23 02:09:23 +0200

    Navbar components
    added navbar components for example search container language chooser etc..

--- my-app/src/Components/Navbar.jsx
+++ my-app/src/Components/Navbar.jsx
@@ -1,8 +1,11 @@
 import React from 'react'
 import "./Navbar.css";
 import styled from 'styled-components'
+import Search from '@mui/icons-material/SearchOutlined';
 
 const First = styled.div``
+const Launguage = styled.span``
+const SearchContainer = styled.div``
 const Second = styled.div``
 const Third = styled.div``
 
@@ -10,9 +13,15 @@ function Navbar() {
   return (
     <div className='navbar-container'>
       <div className='navbar-wrapper'>
-        <First>First</First>
-        <Second>Second</Second>
-        <Third>Third</Third>
+        <First className='navbar-elements-first'>
+            <Launguage className='navbar-language'>HU</Launguage>
+            <SearchContainer className='navbar-searchcontainer'>
+                keresés..
+                <Search/>
+            </SearchContainer>
+        </First>
+        <Second className='navbar-elements-second'>Második</Second>
+        <Third className='navbar-elements-third'>Harmadik</Third>
       </div>
     </div>
   )
