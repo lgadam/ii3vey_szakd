22fd0c2802b446f72cad96b0cf193e32efec1a83
Adam Ligart <ligartadam@gmail.com>
2022-08-23 03:03:05 +0200

    navbar: added register login cart buttons
     added register login cart buttons

--- my-app/src/Components/Navbar.jsx
+++ my-app/src/Components/Navbar.jsx
@@ -2,6 +2,8 @@ import React from 'react'
 import "./Navbar.css";
 import styled from 'styled-components'
 import Search from '@mui/icons-material/SearchOutlined';
+import Badge from '@mui/material/Badge';
+import ShoppingBasket from '@mui/icons-material/ShoppingBasketOutlined';
 
 const First = styled.div``
 const Launguage = styled.span``
@@ -10,6 +12,7 @@ const Input = styled.input``
 const Second = styled.div``
 const Logo = styled.h1``
 const Third = styled.div``
+const MenuItem = styled.div``
 
 function Navbar() {
   return (
@@ -19,11 +22,20 @@ function Navbar() {
             <Launguage className='navbar-language'>HU</Launguage>
             <SearchContainer className='navbar-searchcontainer'>
                 <Input className='navbar-input'/>
-                <Search/>
+                <Search style={{color:"gray", fontSize:15}}/>
             </SearchContainer>
         </First>
         <Second className='navbar-elements-second'><Logo>LgAdam Wines</Logo></Second>
-        <Third className='navbar-elements-third'>Harmadik</Third>
+        <Third className='navbar-elements-third'>
+            <MenuItem className='navbar-third-items'>Regisztráció</MenuItem>
+            <MenuItem className='navbar-third-items'>Bejelentkezés</MenuItem>
+            <MenuItem className='navbar-third-items'>
+                <Badge color="primary">
+                    <ShoppingBasket/>
+                </Badge>
+            </MenuItem>
+
+        </Third>
       </div>
     </div>
   )
