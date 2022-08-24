1d7e407c36b619000692d6a7d684ac0deacb65a9
Adam Ligart <ligartadam@gmail.com>
2022-08-25 01:35:44 +0200

    Added map to use ProductItem in Products
    Added map to use ProductItem in Products

--- my-app/src/Components/Products.jsx
+++ my-app/src/Components/Products.jsx
@@ -1,10 +1,14 @@
 import styled from "styled-components"
 import { advantagedProducts } from "../data"
+import ProductItem from "./ProductItem"
 const Container = styled.div``
 
 const Products = () => {
   return (
     <Container>
+        {advantagedProducts.map(item=>(
+            <ProductItem item={item} key={item.id} />
+        ))}
     </Container>
   )
 }
