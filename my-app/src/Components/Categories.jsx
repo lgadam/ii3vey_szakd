f79d08fd36bce1ab903c1ad3356036ca7abd9c25
Adam Ligart <ligartadam@gmail.com>
2022-08-24 23:46:09 +0200

    create mapping categoryitem to categories
    create mapping categoryitem to categories

--- my-app/src/Components/Categories.jsx
+++ my-app/src/Components/Categories.jsx
@@ -1,11 +1,14 @@
 import styled from 'styled-components'
 import { categories } from '../data'
+import CategoryItem from './CategoryItem'
 const Container = styled.div``
 
 const Categories = () => {
   return (
     <Container>
-      
+      {categories.map(item=>(
+        <CategoryItem item={item}/>
+      ))}
     </Container>
   )
 }
