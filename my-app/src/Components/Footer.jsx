901846974fcc1f50036fdd476a1c4199865bc988
Adam Ligart <ligartadam@gmail.com>
2022-09-05 16:46:15 +0200

    Footer: Added container, and container elements
    Footer: Added container, and container elements

--- my-app/src/Components/Footer.jsx
+++ my-app/src/Components/Footer.jsx
@@ -1,4 +1,17 @@
-import React from 'react'
+import styled from "styled-components"
+
+const Container = styled.div`
+    display: flex;
+`
+const First = styled.div`
+
+`
+const Second = styled.div`
+
+`
+const Third = styled.div`
+
+`
 
 const Footer = () => {
   return (
