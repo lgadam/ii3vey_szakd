a74ddd17f0d738341df3a2ccf4665ec524df1f58
Adam Ligart <ligartadam@gmail.com>
2022-09-08 16:10:36 +0200

    ProductList: Added filterbar select and options
    ProductList: Added filterbar select and options

--- my-app/src/Pages/ProductList.jsx
+++ my-app/src/Pages/ProductList.jsx
@@ -20,6 +20,15 @@ const Filter = styled.div`
 const FilterSearch = styled.span`
     font-size: 18px;
     font-weight: 400;
+    margin-right: 20px;
+`
+const Select = styled.select`
+    padding: 12px;
+    margin-right: 20px;
+    border: 1px solid #24d684;
+`
+const Option = styled.option`
+
 `
 
 const ProductList = () => {
@@ -30,10 +39,23 @@ const ProductList = () => {
         <Title>Termékek</Title>
         <FilterBar>
             <Filter>
-                <FilterSearch>Keresés a termékekre</FilterSearch>
+                <FilterSearch>Keresés a termékekre..</FilterSearch>
+                <Select>
+                    <Option disabled selected>
+                        Borok
+                    </Option>
+                    <Option>Sauvignon</Option>
+                    <Option>Bikavér</Option>
+                    <Option>Porto</Option>
+                </Select>
             </Filter>
             <Filter>
                 <FilterSearch>Rendezés..</FilterSearch>
+                <Select>
+                    <Option selected>Új termékek</Option>
+                    <Option>Ár (csökkenő)</Option>
+                    <Option>Ár (növekvő)</Option>
+                </Select>
             </Filter>
         </FilterBar>
         <Products/>
