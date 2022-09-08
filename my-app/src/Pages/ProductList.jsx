768296ef4a2b257060e86332fc56b859a5ae204a
Adam Ligart <ligartadam@gmail.com>
2022-09-08 15:53:31 +0200

    ProductList: added body components, search and sort
    ProductList: added body components, search and sort

--- my-app/src/Pages/ProductList.jsx
+++ my-app/src/Pages/ProductList.jsx
@@ -1,14 +1,44 @@
 import styled from "styled-components"
 import Navbar from "../Components/Navbar"
 import Announcement from "../Components/Announcement"
+import Products from "../Components/Products"
+import GuideMail from "../Components/GuideMail"
+import Footer from "../Components/Footer"
 const Container = styled.div`
 
 `
+const Title = styled.h1`
+    margin: 30px;
+`
+const FilterBar = styled.div`
+    display: flex;
+    justify-content: space-between;
+`
+const Filter = styled.div`
+    margin: 30px;
+`
+const FilterSearch = styled.span`
+    font-size: 18px;
+    font-weight: 400;
+`
+
 const ProductList = () => {
   return (
     <Container>
         <Announcement />
         <Navbar />
+        <Title>Termékek</Title>
+        <FilterBar>
+            <Filter>
+                <FilterSearch>Keresés a termékekre</FilterSearch>
+            </Filter>
+            <Filter>
+                <FilterSearch>Rendezés..</FilterSearch>
+            </Filter>
+        </FilterBar>
+        <Products/>
+        <GuideMail/>
+        <Footer/>
     </Container>
   )
 }
