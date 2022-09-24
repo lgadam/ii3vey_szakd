bce4a1015f4bd325f6bae934d69a1267a4a15b88
Adam Ligart <ligartadam@gmail.com>
2022-09-24 05:19:23 +0200

    Login page: added form, input fields,button,link
    Login page: added form, input fields,button,link

--- my-app/src/Pages/Login.jsx
+++ my-app/src/Pages/Login.jsx
@@ -6,7 +6,16 @@ const Container = styled.div``
 const Login = () => {
   return (
     <Container>
-      
+      <Wrapper>
+            <Title>Bejelentkezés</Title>
+            <Form>
+                <Input placeholder="felhasználónév" />
+                <Input placeholder="jelszó" />
+                <Button className="btn-slider">Belépés</Button>
+                <Link>Elfelejtetted a jelszavad? itt kérhetsz emlékeztetőt</Link>
+                <Link>Ha még nincs fiókod, itt regisztrálhatsz</Link>
+            </Form>
+        </Wrapper>
     </Container>
   )
 }
