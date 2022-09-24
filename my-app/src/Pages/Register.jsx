daf0eb2dd08d0cf8662ff60d816462f790a39fee
Adam Ligart <ligartadam@gmail.com>
2022-09-24 03:20:25 +0200

    Register page: added inputs to the form
    Register page: added inputs to the form

--- my-app/src/Pages/Register.jsx
+++ my-app/src/Pages/Register.jsx
@@ -1,11 +1,21 @@
 import styled from "styled-components"
 
 const Container = styled.div``
-
+const Wrapper = styled.div``
 const Register = () => {
   return (
     <Container>
-      
+        <Wrapper>
+            <Title>Regisztráció</Title>
+            <Form>
+                <Input placeholder="felhasználónév" />
+                <Input placeholder="keresztnév" />
+                <Input placeholder="vezetéknév" />
+                <Input placeholder="email cím" />
+                <Input placeholder="jelszó" />
+                <Input placeholder="jelszó újra" />
+            </Form>
+        </Wrapper>
     </Container>
   )
 }
