c81142f972e75095e61200815aae5b5aa933e38d
Adam Ligart <ligartadam@gmail.com>
2022-09-24 03:30:39 +0200

    Register page: added privacy and confirm button
    Register page: added privacy and confirm button

--- my-app/src/Pages/Register.jsx
+++ my-app/src/Pages/Register.jsx
@@ -1,7 +1,13 @@
 import styled from "styled-components"
+import "../Components/Slider.css"
 
 const Container = styled.div``
 const Wrapper = styled.div``
+const Privacy = styled.span``
+const Title = styled.h1``
+const Form = styled.form``
+const Button = styled.button``
+const Input = styled.input``
 const Register = () => {
   return (
     <Container>
@@ -14,6 +20,8 @@ const Register = () => {
                 <Input placeholder="email cím" />
                 <Input placeholder="jelszó" />
                 <Input placeholder="jelszó újra" />
+                <Privacy>A létrehozás során a felhasználónak elkell fogadni a szabályzatot. <italy>Szabályzat</italy></Privacy>
+                <Button className="btn-slider">Felhasználó létrehozása</Button>
             </Form>
         </Wrapper>
     </Container>
