7aa661ef987c60067f71877336b8a3e89bcfb135
Adam Ligart <ligartadam@gmail.com>
2022-09-30 01:56:05 +0200

    CartPage: added amount container, price datas

--- my-app/src/Pages/Cart.jsx
+++ my-app/src/Pages/Cart.jsx
@@ -4,6 +4,8 @@ import Announcement from '../Components/Announcement'
 import styled from 'styled-components'
 import "../Components/Slider.css"
 import bor from '../advbor1.png';
+import Delete from '@mui/icons-material/Remove';
+import Add from '@mui/icons-material/Add';
 const Container = styled.div``
 const Wrapper = styled.div`
     padding: 20px;
@@ -45,6 +47,10 @@ const ProductData = styled.div`
 `
 const PriceData = styled.div`
     flex: 1;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
 `
 const Image = styled.img`
     width: 200px;
@@ -59,6 +65,27 @@ const Datas = styled.div`
 const ProductName = styled.span``
 const ProductId = styled.span``
 const ProductType = styled.span``
+const ProductAmountContainer = styled.div`
+    display: flex;
+    align-items: center;
+    margin-bottom: 30px;
+`
+const ProductPrice = styled.div``
+const Amount = styled.span`
+    height: 50px;
+    width: 50px;
+    border-radius: 12px;
+    border: 1px solid #1a995d;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    margin: 0px 5px;
+`
+const Hr = styled.hr`
+    background-color: #eee;
+    border: none;
+    height: 2px;
+`
 const Cart = () => {
   return (
     <Container>
@@ -86,7 +113,31 @@ const Cart = () => {
                             </Datas>
                         </ProductData>
                         <PriceData>
-                            Ár
+                            <ProductAmountContainer>
+                                <Delete />
+                                <Amount>1</Amount>
+                                <Add />
+                            </ProductAmountContainer>
+                            <ProductPrice>1200 ft</ProductPrice>
+                        </PriceData>
+                    </Product>
+                    <Hr/>
+                    <Product>
+                        <ProductData>
+                            <Image src={bor} />
+                            <Datas>
+                                <ProductName><b>Termék:</b>Lafiesta fruit</ProductName>
+                                <ProductId><b>Azonosító:</b>12345678</ProductId>
+                                <ProductType><b>Fajta:</b>fehér bor</ProductType>  
+                            </Datas>
+                        </ProductData>
+                        <PriceData>
+                            <ProductAmountContainer>
+                                <Delete />
+                                <Amount>1</Amount>
+                                <Add />
+                            </ProductAmountContainer>
+                            <ProductPrice>1200 ft</ProductPrice>
                         </PriceData>
                     </Product>
                 </Information>
