036bd98b56a85633025d3cfc8cf1a7a72fd297c8
Adam Ligart <ligartadam@gmail.com>
2022-09-30 01:36:44 +0200

    Cart page: added information container, image, datas.
    Cart page: added information container, image, datas.

--- my-app/src/Pages/Cart.jsx
+++ my-app/src/Pages/Cart.jsx
@@ -3,6 +3,7 @@ import Footer from '../Components/Footer'
 import Announcement from '../Components/Announcement'
 import styled from 'styled-components'
 import "../Components/Slider.css"
+import bor from '../advbor1.png';
 const Container = styled.div``
 const Wrapper = styled.div`
     padding: 20px;
@@ -34,6 +35,30 @@ const Information = styled.div`
 const Summary = styled.div`
     flex: 1;
 `
+const Product = styled.div`
+    display: flex;
+    justify-content: space-between;
+`
+const ProductData = styled.div`
+    flex: 2;
+    display: flex;
+`
+const PriceData = styled.div`
+    flex: 1;
+`
+const Image = styled.img`
+    width: 200px;
+    height: 200px;
+`
+const Datas = styled.div`
+    padding: 20px;
+    display: flex;
+    flex-direction: column;
+    justify-content: space-around;
+`
+const ProductName = styled.span``
+const ProductId = styled.span``
+const ProductType = styled.span``
 const Cart = () => {
   return (
     <Container>
@@ -50,7 +75,21 @@ const Cart = () => {
                 <BeginBtn className='btn-slider'>Fizetés</BeginBtn>
             </Begin>
             <End>
-                <Information>information</Information>
+                <Information>
+                    <Product>
+                        <ProductData>
+                            <Image src={bor} />
+                            <Datas>
+                                <ProductName><b>Termék:</b>Lafiesta fruit</ProductName>
+                                <ProductId><b>Azonosító:</b>12345678</ProductId>
+                                <ProductType><b>Fajta:</b>fehér bor</ProductType>  
+                            </Datas>
+                        </ProductData>
+                        <PriceData>
+                            Ár
+                        </PriceData>
+                    </Product>
+                </Information>
                 <Summary>summary</Summary>
             </End>
         </Wrapper>
