0580c4d808c350f7b4bfd6cff4c4f45f397032ce
Adam Ligart <ligartadam@gmail.com>
2022-10-01 08:18:57 +0200

    Router: register post method edited, with status response
    Router: register post method edited, with status response

--- backend/index.js
+++ backend/index.js
@@ -3,7 +3,7 @@ const app = express();
 const mongoose = require("mongoose");
 const dotenv = require("dotenv");
 const userRoute = require("./routes/user");
-
+const authRoute = require("./routes/auth");
 dotenv.config();
 
 mongoose.connect(process.env.MONGO_URL)
@@ -13,6 +13,7 @@ mongoose.connect(process.env.MONGO_URL)
 
 app.use(express.json());
 app.use("/api/users", userRoute);
+app.use("/api/auth", authRoute);
 
 app.listen(process.env.PORT || 8000, ()=> {
     console.log("Szerver fut");
--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -1,3 +1,17 @@
 const router = require("express").Router();
-
-module.exports = router
+const User = require("../models/User");
+//regisztráció
+router.post("/register", async (req,res)=>{
+    const newUser = new User({
+        username: req.body.username,
+        email: req.body.email,
+        password: req.body.password,
+    });
+    try{
+        const savedUser = await newUser.save(); // save method használata az adatbázisba mentéshez.
+        res.status(201).json(savedUser)
+    }catch(err){
+        res.status(500).json(err);
+    }
+});
+module.exports = router;
