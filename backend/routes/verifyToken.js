2dfe3b10767afe662a41432940b44d9cee1ae88d
Adam Ligart <ligartadam@gmail.com>
2022-10-01 14:58:36 +0200

    Router: fixed token verify, username update function
    Router: fixed token verify, username update function

---
+++ backend/routes/verifyToken.js
@@ -0,0 +1,27 @@
+const jwt = require("jsonwebtoken");
+
+const verifyToken = (req, res, next) => {
+    const authHeader = req.headers.token;
+    if (authHeader) {
+      const token = authHeader.split(" ")[1];
+      jwt.verify(token, process.env.JWT_SECRT, (err, user) => {
+        if (err) res.status(403).json("A token nem valós");
+        req.user = user;
+        next();
+      });
+    } else {
+      return res.status(401).json("A felhasználó nincs hitelesítve");
+    }
+  };
+  
+  const verifyTokenAndAuthorization = (req, res, next) => {
+    verifyToken(req, res, () => {
+      if (req.user.id === req.params.id || req.user.isAdmin) {
+        next();
+      } else {
+        res.status(403).json("Nincs jogosultság a következő műveletekhez");
+      }
+    });
+  };
+
+module.exports = {verifyToken,verifyTokenAndAuthorization};
