b707df08adf5a336c4004d50071cfbcbf05d9cd7
Adam Ligart <ligartadam@gmail.com>
2022-10-01 06:41:42 +0200

    added routes folder, create user.js and test endpoint
    added routes folder, create user.js and test endpoint

--- backend/index.js
+++ backend/index.js
@@ -1,10 +1,8 @@
 const express = require("express");
-
 const app = express();
-
 const mongoose = require("mongoose");
-
 const dotenv = require("dotenv");
+const userRoute = require("./routes/user");
 
 dotenv.config();
 
@@ -12,6 +10,8 @@ mongoose.connect(process.env.MONGO_URL)
 .then(()=>console.log("Adatbázis kapcsolódás sikeres")).catch((err)=>{
     console.log(err)
 });
+
+app.use("/api/user", userRoute);
 
 app.listen(process.env.PORT || 8000, ()=> {
     console.log("Szerver fut");
