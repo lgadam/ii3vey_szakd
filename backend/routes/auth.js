b37ca990fb3e1bb79df6bf84748b49aff887bd2a
Adam Ligart <ligartadam@gmail.com>
2022-10-01 10:43:52 +0200

    Authentication:  decrypt password and compared, login function
    Authentication:  decrypt password and compared, login function

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -1,13 +1,12 @@
 const router = require("express").Router();
 const User = require("../models/User");
-const dotenv = require("dotenv");
-const crypto_js = require("crypto-js");
+const CryptoJS = require("crypto-js");
 //regisztráció
 router.post("/register", async (req,res)=>{
     const newUser = new User({
         username: req.body.username,
         email: req.body.email,
-        password: crypto_js.AES.encrypt(req.body.password,process.env.PASSWD_SECRT).toString(),
+        password: CryptoJS.AES.encrypt(req.body.password,process.env.PASSWD_SECRT).toString(),
     });
     try{
         const savedUser = await newUser.save(); // save method használata az adatbázisba mentéshez.
@@ -16,4 +15,23 @@ router.post("/register", async (req,res)=>{
         res.status(500).json(err);
     }
 });
+//bejelentkezés
+router.post("/login", async (req,res) =>{
+    try{
+        const user = await User.findOne({ username: req.body.username });
+        if(!user) return res.status(401).json("Helytelen adatok");
+        const HashedPassword = CryptoJS.AES.decrypt(
+            user.password,
+            process.env.PASSWD_SECRT
+        );
+        const password = HashedPassword.toString(CryptoJS.enc.Utf8);
+        if(password !== req.body.password) return res.status(401).json("Helytelen adatok");
+
+        res.status(200).json(user);
+    }catch(err){
+        res.status(500).json(err);    
+    }
+     
+})
+
 module.exports = router;
