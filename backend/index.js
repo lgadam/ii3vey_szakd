acedbbdb91efd672ecefb1f7374e611760dc3ae3
Adam Ligart <ligartadam@gmail.com>
2022-10-01 06:29:14 +0200

    added dotenv file, edited mongodb connection url
    added dotenv file, edited mongodb connection url

--- backend/index.js
+++ backend/index.js
@@ -4,12 +4,15 @@ const app = express();
 
 const mongoose = require("mongoose");
 
-mongoose.connect(
-    "mongodb+srv://lgadam:foltos344@cluster0.tv1qglk.mongodb.net/shop?retryWrites=true&w=majority"
-).then(()=>console.log("Adatbázis kapcsolódás sikeres")).catch((err)=>{
+const dotenv = require("dotenv");
+
+dotenv.config();
+
+mongoose.connect(process.env.MONGO_URL)
+.then(()=>console.log("Adatbázis kapcsolódás sikeres")).catch((err)=>{
     console.log(err)
 });
 
-app.listen(8000, ()=> {
+app.listen(process.env.PORT || 8000, ()=> {
     console.log("Szerver fut");
 });
