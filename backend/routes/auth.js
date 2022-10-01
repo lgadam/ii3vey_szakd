c5973b812acc52c5fc6c177c103e7b4f40473ee6
Adam Ligart <ligartadam@gmail.com>
2022-10-01 08:37:56 +0200

    Authentication: added cryptojs module, AES password encrypt
    Authentication: added cryptojs module, AES password encrypt

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -1,11 +1,13 @@
 const router = require("express").Router();
 const User = require("../models/User");
+const dotenv = require("dotenv");
+const crypto_js = require("crypto-js");
 //regisztráció
 router.post("/register", async (req,res)=>{
     const newUser = new User({
         username: req.body.username,
         email: req.body.email,
-        password: req.body.password,
+        password: crypto_js.AES.encrypt(req.body.password,process.env.PASSWD_SECRT).toString(),
     });
     try{
         const savedUser = await newUser.save(); // save method használata az adatbázisba mentéshez.
