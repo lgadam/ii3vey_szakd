3cf6760be31c95d4eb68b1b33484d0654596abe2
Adam Ligart <ligartadam@gmail.com>
2022-10-04 00:41:36 +0200

    stripe Route: added payment backend with cors
    stripe Route: added payment backend with cors

--- backend/index.js
+++ backend/index.js
@@ -7,19 +7,24 @@ const authRoute = require("./routes/auth");
 const productRoute = require("./routes/product");
 const cartRoute = require("./routes/cart");
 const orderRoute = require("./routes/order");
+const cors = require("cors");
 dotenv.config();
+const stripeRoute = require("./routes/stripe");
 
 mongoose.connect(process.env.MONGO_URL)
 .then(()=>console.log("Adatbázis kapcsolódás sikeres")).catch((err)=>{
     console.log(err)
 });
 
+
 app.use(express.json());
+app.use(cors());
 app.use("/api/users", userRoute);
 app.use("/api/auth", authRoute);
 app.use("/api/products", productRoute);
 app.use("/api/carts", cartRoute);
 app.use("/api/orders", orderRoute);
+app.use("/api/checkout", stripeRoute);
 
 app.listen(process.env.PORT || 8000, ()=> {
     console.log("Szerver fut");
