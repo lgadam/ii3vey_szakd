0dff56575611eb43499eaaad06e986ed1dfa4d0d
Adam Ligart <ligartadam@gmail.com>
2022-10-03 10:30:10 +0200

    Order route: added Create,Update,Delete methods
    Order route: added Create,Update,Delete methods

--- backend/routes/order.js
+++ backend/routes/order.js
@@ -1,3 +1,46 @@
 const router = require("express").Router();
 
+const Order = require("../models/Order");
+const { verifyToken,verifyTokenAndAuthorization, verifyTokenAndAdmin } = require("./verifyToken");
+
+//Create
+
+router.post("/", verifyToken, async (req,res) =>{
+    const newOrder = new Order(req.body);
+    try{
+        const savedOrder = await newOrder.save();
+        res.status(200).json(savedOrder);
+    }catch(err){
+        res.status(500).json(err)
+    }
+});
+
+//Update
+
+router.put("/:id", verifyTokenAndAdmin, async (req, res) => {
+    try {
+      const updatedOrder= await Order.findByIdAndUpdate(
+        req.params.id,
+        {
+          $set: req.body,
+        },
+        { new: true }
+      );
+      res.status(200).json(updatedOrder);
+    } catch (err) {
+      res.status(500).json(err);
+    }
+});
+
+//Delete
+
+router.delete("/:id", verifyTokenAndAdmin, async (req,res)=>{
+    try{
+      await Order.findByIdAndDelete(req.params.id);
+      res.status(200).json("Rendelés kitörölve");
+    }catch(err){
+      res.status(500).json(err);
+    }
+});
+
 module.exports = router
