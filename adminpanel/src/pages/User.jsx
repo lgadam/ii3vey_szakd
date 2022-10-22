89e9f9b89674bcb9d5fb24b50b22571ea7b77f05
Adam Ligart <ligartadam@gmail.com>
2022-10-22 12:01:07 +0200

    User page: added upload picture option, edit container
    User page: added upload picture option, edit container

--- adminpanel/src/pages/User.jsx
+++ adminpanel/src/pages/User.jsx
@@ -1,4 +1,11 @@
 import styled from "styled-components"
+import AccountBoxIcon from '@mui/icons-material/AccountBox';
+import WatchLaterIcon from '@mui/icons-material/WatchLater';
+import PhoneIphoneIcon from '@mui/icons-material/PhoneIphone';
+import MailIcon from '@mui/icons-material/Mail';
+import PlaceIcon from '@mui/icons-material/Place';
+import UploadFileIcon from '@mui/icons-material/UploadFile';
+import logo from '../components/navbar/panel_logo.PNG';
 
 const Container = styled.div`
     flex: 4;
@@ -48,7 +55,10 @@ const UserDisplayTop = styled.div`
 
 const UserDisplayBottom = styled.div``
 
-const DisplayTopTitle = styled.div``
+const DisplayTopTitle = styled.div`
+    display: flex;
+    flex-direction: column;
+`
 
 const DisplayTopUsername = styled.span`
     font-weight: 700;
@@ -56,6 +66,74 @@ const DisplayTopUsername = styled.span`
 
 const DisplayTopUserTitle = styled.span`
     font-weight: 200;
+`
+const DisplayTitle = styled.span`
+    font-size: 16px;
+    font-weight: 600;
+    color: #6666;
+`
+
+const DisplayInfoTitle = styled.span`
+    margin-left: 10px;
+
+`
+
+const DisplayInfo = styled.div`
+    display: flex;
+    align-items: center;
+    margin: 20px 0px;
+`
+const UserUpdateTitle = styled.span`
+    font-size: 24px;
+    font-weight: 600;
+`
+
+const UserUpdateForm = styled.form`
+    display: flex;
+    justify-content: space-between;
+    margin-top: 20px;
+`
+
+const UserUpdateLeft = styled.div``
+
+const UserUpdateItem = styled.div`
+    display: flex;
+    flex-direction: column;
+    margin-top:10px;
+`
+
+const Label = styled.label`
+    margin-bottom: 5px;
+    font-size: 14px;
+`
+
+const Input = styled.input`
+    height: 25px;
+    border: none;
+    width: 250px;
+    border-bottom: 1px solid gray;
+`
+
+const UserUpdateRight = styled.div`
+    display: flex;
+    align-items: center;
+    flex-direction: column;
+`
+
+const UserUpdateImg = styled.img`
+    width: 100px;
+    height: 100px;
+    border-radius: 10px;
+    object-fit: cover;
+    margin-right: 20px;
+`
+const UpdateButton = styled.button`
+    border-radius: 5px;
+    border:none;
+    padding: 5px;
+    cursor: pointer;
+    background-color:#6fdcab;
+
 `
 
 export default function User() {
@@ -73,9 +151,62 @@ export default function User() {
                         <DisplayTopUserTitle>Programtervező informatikus</DisplayTopUserTitle>
                     </DisplayTopTitle>
                 </UserDisplayTop>
-                <UserDisplayBottom></UserDisplayBottom>
+                <UserDisplayBottom>
+                    <DisplayTitle>Adatok:</DisplayTitle>
+                    <DisplayInfo>
+                        <AccountBoxIcon className="user-icon"/>
+                        <DisplayInfoTitle>lgadam</DisplayInfoTitle>
+                    </DisplayInfo>
+                    <DisplayInfo>
+                        <WatchLaterIcon className="user-icon"/>
+                        <DisplayInfoTitle>2000.11.24</DisplayInfoTitle>
+                    </DisplayInfo>
+                    <DisplayInfo>
+                        <PhoneIphoneIcon className="user-icon"/>
+                        <DisplayInfoTitle>+36 20 123 4567</DisplayInfoTitle>
+                    </DisplayInfo>
+                    <DisplayInfo>
+                        <MailIcon className="user-icon"/>
+                        <DisplayInfoTitle>lgadam@gmail.com</DisplayInfoTitle>
+                    </DisplayInfo>
+                    <DisplayInfo>
+                        <PlaceIcon className="user-icon"/>
+                        <DisplayInfoTitle>3348 Szilvásvárad, Hungary</DisplayInfoTitle>
+                    </DisplayInfo>
+                </UserDisplayBottom>
             </UserDisplay>
             <UserUpdate>
+                <UserUpdateTitle>Szerkesztés</UserUpdateTitle>
+                <UserUpdateForm>
+                    <UserUpdateLeft>
+                        <UserUpdateItem>
+                            <Label>Felhasználónév</Label>
+                            <Input type="text" placeholder="felhasználónév"></Input>
+                        </UserUpdateItem>
+                        <UserUpdateItem>
+                            <Label>Teljes név</Label>
+                            <Input type="text" placeholder="teljesnév"></Input>
+                        </UserUpdateItem>
+                        <UserUpdateItem>
+                            <Label>Email</Label>
+                            <Input type="text" placeholder="email"></Input>
+                        </UserUpdateItem>
+                        <UserUpdateItem>
+                            <Label>Telefonszám</Label>
+                            <Input type="text" placeholder="Telefonszám"></Input>
+                        </UserUpdateItem>
+                        <UserUpdateItem>
+                            <Label>Lakcím</Label>
+                            <Input type="text" placeholder="Lakcím"></Input>
+                        </UserUpdateItem>
+                    </UserUpdateLeft>
+                    <UserUpdateRight>
+                        <UserUpdateImg img src={logo}/>
+                        <Label htmlFor="file"><UploadFileIcon /></Label>
+                        <Input type="file" id="file" style={{display: "none"}}></Input>
+                        <UpdateButton>Feltöltés</UpdateButton> 
+                    </UserUpdateRight>  
+                </UserUpdateForm>
             </UserUpdate>
         </UserContainer>
     </Container>
